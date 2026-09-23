c840f92e69057f3acba4cbc33f678f43d61b553f
guntisungurs <g.ungurs2003@gmail.com>
2026-09-23 09:49:19 +0300

    Vārds Uzvārds vēcums

--- faili.cpp
+++ faili.cpp
@@ -4,7 +4,21 @@ using namespace std;
 
 int main(){ 
     ofstream fails("dati.txt");
-    fails << "Sveiki!"<<endl;
+    cout<<"Ievadi savu vārdu!"<<endl;
+    string name;
+    cin >> name;
+
+    cout<<"Ievadi savu uzvārdu!"<<endl;
+    string last_name;
+    cin >> last_name;
+
+    cout<<"Ievadi savu vecumu!"<<endl;
+    int age;
+    cin >> age;
+
+    fails <<"Vārds: "<<name<<endl;
+    fails <<"Uzvārds: "<<last_name<<endl;
+    fails <<"Vecums: "<<age<<endl;
     fails.close();
     return 0;
 }
